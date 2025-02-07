f8117b6efbc9d04d1c2f87dd9a91a4d0f1454992
Thec17 <137742154+Thec17@users.noreply.github.com>
2025-02-07 18:55:32 -0500

    Update main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -20,3 +20,8 @@ async def root():
 @app.get("/url")
 async def url():
     return {"url": "https://mouredev.com/python"}
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8000))  # Usa el puerto que Railway asigna
+    uvicorn.run(app, host="0.0.0.0", port=port)
